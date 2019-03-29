9ac4a0be9e7b2f75c153c13c2e5f3018a69772e4
Pat P <sell445@gmail.com>
2019-03-29 09:48:58 -0400

    Added background and styling to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,36 +3,48 @@ import '../App.css';
 import { NavLink } from 'react-router-dom';
 
 
-function Home() {
-  return (
-    <div id="homeDiv">
-        <div className="columnDiv">
-          <h2>Name Generators</h2>
-          <h3>Location Names</h3>
-          <NavLink className="navLink" to="/forest-name-generator">Forest Name Generator</NavLink><br />
-          <NavLink className="navLink" to="/mountain-range-generator">Mountain Range Generator</NavLink><br />
-          <NavLink className="navLink" to="/river-name-generator">River Name Generator</NavLink><br />
-          <h3>Character Names</h3>
-          <NavLink className="navLink" to="/fantasy-surname-generator">Fantasy Surname Generator</NavLink><br />
-          <NavLink className="navLink" to="/eldritch-name-generator">Eldritch Name Generator</NavLink><br />
-          <NavLink className="navLink" to="/elf-name-generator">Elf Name Generator</NavLink><br />
-          <NavLink className="navLink" to="/orc-name-generator">Orc Name Generator</NavLink><br />
-          <NavLink className="navLink" to="/dwarf-name-generator">Dwarf Name Generator</NavLink><br />
+class Home extends React.Component {
+
+  componentDidMount() {
+    document.body.classList.add('home')
+  }
+
+  componentWillUnmount() {
+    document.body.classList.remove('home')
+  }
+
+  render() {
+    return (
+      <div id="homeDiv">
+          <div className="columnDiv">
+            <h2>Name Generators</h2>
+            <h3>Location Names</h3>
+            <NavLink className="navLink" to="/forest-name-generator">Forest Name Generator</NavLink><br />
+            <NavLink className="navLink" to="/mountain-range-generator">Mountain Range Generator</NavLink><br />
+            <NavLink className="navLink" to="/river-name-generator">River Name Generator</NavLink><br />
+            <h3>Character Names</h3>
+            <NavLink className="navLink" to="/fantasy-surname-generator">Fantasy Surname Generator</NavLink><br />
+            <NavLink className="navLink" to="/eldritch-name-generator">Eldritch Name Generator</NavLink><br />
+            <NavLink className="navLink" to="/elf-name-generator">Elf Name Generator</NavLink><br />
+            <NavLink className="navLink" to="/orc-name-generator">Orc Name Generator</NavLink><br />
+            <NavLink className="navLink" to="/dwarf-name-generator">Dwarf Name Generator</NavLink><br />
 
 
-          <h2>Other Generators</h2>
-          <NavLink className="navLink" to="/movie-personality-mashup">Movie Personality Mashup</NavLink>
+            <h2>Other Generators</h2>
+            <NavLink className="navLink" to="/movie-personality-mashup">Movie Personality Mashup</NavLink>
 
-        </div>
-        <div className="columnDiv">
-          <h2>Generators by System</h2>
-          <h3>Dungeon World</h3>
-          <NavLink className="navLink" to="/dungeon-world-gm-tools">Game Master Tools</NavLink>
-          <h3>Freebooters on the Frontier</h3>
-          <NavLink className="navLink" to="/freebooters-character-generator">Character Generator</NavLink>
-        </div>
-    </div>
-  )
+          </div>
+          <div className="columnDiv">
+            <h2>Generators by System</h2>
+            <h3>Dungeon World</h3>
+            <NavLink className="navLink" to="/dungeon-world-gm-tools">Game Master Tools</NavLink>
+            <h3>Freebooters on the Frontier</h3>
+            <NavLink className="navLink" to="/freebooters-character-generator">Character Generator</NavLink>
+          </div>
+      </div>
+    )
+
+  }
 }
 
 export default Home
